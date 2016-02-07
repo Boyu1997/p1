520e842fcae01ece5f8a808e947a67f114ee1b1f
Jiang Boyu <jbyjiangboyu@126.com>
2016-02-07 16:13:55 +0800

    First commit of p1.

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+
+  <head>
+    <title>p1</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+  </head>
+
+  <body>
+
+    <h1>Jiang Boyu</h1>
+    <img src="./images/Boyu.jpeg">
+
+    <h2>Project 2 Title</h2>
+    <p>View: [link]</p>
+    <p>Github: [link]</P>
+
+    <h2>Project 3 Title</h2>
+    <p>View: [link]</p>
+    <p>Github: [link]</P>
+
+    <h2>Project 4 Title</h2>
+    <p>View: [link]</p>
+    <p>Github: [link]</P>
+  </body>
+
+</html>
